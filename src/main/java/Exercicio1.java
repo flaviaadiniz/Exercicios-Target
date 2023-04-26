07ddf3e85457db6446da0bb07392177364f38286
flaviaadiniz <flaviaadiniz@gmail.com>
2023-04-26 13:22:12 -0300

    Adição das respostas dos exercícios 1, 3 e 5.

--- src/main/java/Exercicio1.java
+++ src/main/java/Exercicio1.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Exercicio1 {
+/*
+1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+
+imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA?
+RESPOSTA: 91
+
+ */
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+
+
+        while (k < indice) {
+            k += 1;
+            soma += k;
+        }
+
+        System.out.println(soma); //RESULTADO: 91
+
+    }
+
 }
--- src/main/java/Exercicio5.java
+++ src/main/java/Exercicio5.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Exercicio5 {
+/*
+5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente
+definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
+ */
+
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+
+        String string1 = "Keep Coding!";
+        StringBuilder string2 = new StringBuilder();
+
+        for (int i = string1.length() - 1 ; i >= 0; i--) {
+            string2.append(string1.charAt(i));
+        }
+
+        System.out.println(string2);
+
+    }
+
 }
