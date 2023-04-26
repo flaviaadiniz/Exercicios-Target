ee1e9826965c3a1d865fca580d1465a4aabdd2e4
flaviaadiniz <flaviaadiniz@gmail.com>
2023-04-26 14:39:01 -0300

    Adição das respostas dos exercícios 2 e 4.

--- src/main/java/Exercicio2.java
+++ src/main/java/Exercicio2.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Exercicio2 {
+/*
+Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores
+anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar
+onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o
+número informado pertence ou não a sequência.
+ */
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        int n1 = 0;
+        int n2 = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para descobrir se ele pertence à sequência Fibonacci: ");
+        int num = scanner.nextInt();
+
+        int[] fibonacci = new int[num+3];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i <= fibonacci.length - 1; i++) {
+            fibonacci[i] = n1 + n2;
+            n1 = n2;
+            n2 = fibonacci[i];
+        }
+
+        for (int i : fibonacci) {
+            System.out.println(i);
+        }
+
+        boolean resposta = false;
+        for (int i : fibonacci) {
+            if (i == num) {
+                resposta = true;
+            }
+        }
+
+        if (resposta) {
+            System.out.println("O número " + num + " pertence à sequência Fibonacci.");
+        } else {
+            System.out.println("O número " + num + " NÃO pertence à sequência Fibonacci.");
+        }
+
+        scanner.close();
+
+    }
+
+
 }
--- src/main/java/Exercicio4.java
+++ src/main/java/Exercicio4.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Exercicio4 {
+public class Exercicio4 {
+
+/*
+Dois veículos (um carro e um caminhão) saem respectivamente de cidades opostas pela mesma rodovia. O carro de
+Ribeirão Preto em direção a Franca, a uma velocidade constante de 110 km/h e o caminhão de Franca em direção a
+Ribeirão Preto a uma velocidade constante de 80 km/h. Quando eles se cruzarem na rodovia, qual estará mais
+próximo à cidade de Ribeirão Preto?
+
+IMPORTANTE:
+a) Considerar a distância de 100km entre a cidade de Ribeirão Preto <-> Franca.
+b) Considerar 2 pedágios como obstáculo e que o caminhão leva 5 minutos a mais para passar em cada um deles e o carro possui tag de pedágio (Sem Parar)
+c) Explique como chegou no resultado.
+
+
+RESPOSTA: Os dois veículos encontram-se na mesma distância de Ribeirão Preto, pois estão na mesma posição.
+
+Carro:
+Velocidade média 110km/h
+Posição 0km
+
+Caminhão:
+Velocidade média 70.6km/h (80km/h descontados os 10min perdidos nos pedágios)
+Posição -100km
+
+
+
+*/
+
+
+
+
 }
